97a3be06d59a5a1e0666099b026a2d8892ae5e4d
Rui Wang <rui.wang@databricks.com>
2025-04-25 15:44:56 -0700

    uopdate

--- connectors/spark/src/main/scala/io/unitycatalog/spark/UCSingleCatalog.scala
+++ connectors/spark/src/main/scala/io/unitycatalog/spark/UCSingleCatalog.scala
@@ -172,14 +172,14 @@ class UCSingleCatalog
 
   override def stageCreate(ident: Identifier, columns: Array[Column], partitions: Array[Transform], properties: java.util.Map[String, String]): StagedTable = {
     val oldProperties = loadTableProperties(ident, properties)
-    val newTable = super.createTable(ident, columns, partitions, oldProperties ++ properties)
+    val newTable = createTable(ident, columns, partitions, oldProperties ++ properties)
     BestEffortStagedTable(ident, Option(newTable).getOrElse(loadTable(ident)), this)
   }
 
   override def stageReplace(ident: Identifier, columns: Array[Column], partitions: Array[Transform], properties: java.util.Map[String, String]): StagedTable = {
     val oldProperties = loadTableProperties(ident, properties)
     this.dropTable(ident)
-    val newTable = super.createTable(ident, columns, partitions, oldProperties ++ properties)
+    val newTable = createTable(ident, columns, partitions, oldProperties ++ properties)
     BestEffortStagedTable(ident, Option(newTable).getOrElse(loadTable(ident)), this)
   }
 
@@ -189,7 +189,7 @@ class UCSingleCatalog
     catch {
       case _: NoSuchTableException => // this is fine
     }
-    val newTable = super.createTable(ident, columns, partitions, oldProperties ++ properties)
+    val newTable = createTable(ident, columns, partitions, oldProperties ++ properties)
     BestEffortStagedTable(ident, Option(newTable).getOrElse(loadTable(ident)), this)
   }
 
@@ -197,7 +197,7 @@ class UCSingleCatalog
     if (UCSingleCatalog.DELTA_CATALOG_LOADED.get() &&
       properties.get("provider").equalsIgnoreCase("delta")) {
       try {
-        this.loadTable(ident).properties()
+        loadTable(ident).properties()
       } catch {
         case _: Exception => new util.HashMap[String, String]()
       }
